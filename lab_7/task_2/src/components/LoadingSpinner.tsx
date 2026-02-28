0292dd296df1a6ebcbecd74d86410d9c21b49443
ComeAlongWithMeDoll <ghrothisdeadgod@gmail.com>
2026-02-28 09:48:36 +0600

    Removed wrong folder

--- lab_7/task_2/src/components/LoadingSpinner.tsx
+++
@@ -1,8 +0,0 @@
-export default function LoadingSpinne() {
-    return (
-        <div className="spinner-container">
-            <div className="spinner"></div>
-            <p> Loading page... </p>
-        </div>
-    );
-}
